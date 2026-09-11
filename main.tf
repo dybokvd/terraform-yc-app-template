eac0af16379ddde780e2f8528cf85942465bd34e
Vladislav Dybok <eden91hazard@gmail.com>
2026-09-11 23:41:05 +0300

    feat: add a virtual machine

--- main.tf
+++ main.tf
@@ -9,5 +9,42 @@ terraform {
 }
 
 provider "yandex" {
+  # Default availability zone for resources that don't specify one explicitly
   zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name = "terraform-yc-app-template-vm"
+
+  resources {
+    cores  = 2
+    memory = 4 # GB
+  }
+
+  scheduling_policy {
+    # Prevent unexpected resource termination by Yandex Cloud
+    preemptible = false
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd806u1okplml22f4pmo" # Ubuntu 22.04 LTS image
+      size     = 10                     # GB
+      type     = "network-hdd"
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9b6ninrcea75f4ors6f"
+    # Assign a dynamic public IP address
+    nat = true
+  }
+
+  metadata = {
+    # Public key installed for the "ubuntu" user via cloud-init
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  # Allow Terraform to stop the resource if needed when applying changes
+  allow_stopping_for_update = true
 }
