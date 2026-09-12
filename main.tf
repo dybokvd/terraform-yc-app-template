d535a7cb5774a38961a5137d7fc62c0a8e9cd0c0
Vladislav Dybok <eden91hazard@gmail.com>
2026-09-12 14:11:53 +0300

    feat: configure remote S3-compatible storage as a backend for Terraform state

--- main.tf
+++ main.tf
@@ -6,6 +6,23 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  # S3 backend works here because Yandex Object Storage is S3-compatible
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+
+    bucket = "dibok396-terraform-states-bucket"
+    region = "ru-central1"
+    key    = "terraform-yc-app-template/production/terraform.tfstate"
+
+    # Skip AWS-specific checks that do not apply to Yandex Object Storage
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
